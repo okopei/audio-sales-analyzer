62c5a0225e5ea051bd1f865082809fa0f11fea54
okopei <89885458+okopei@users.noreply.github.com>
2025-01-19 15:47:40 +0900

    fix
    fix

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '',
+  assetPrefix: '',
+  output: 'standalone'
 }
 
 module.exports = nextConfig 
