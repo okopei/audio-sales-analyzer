2d057c40da35fd4438f0d21c74504b5bd6207732
okopei <89885458+okopei@users.noreply.github.com>
2025-01-19 16:34:41 +0900

    Final fix deploy for vercel
    Final fix deploy for vercel

--- next-app/src/app/layout.tsx
+++ next-app/src/app/layout.tsx
@@ -16,9 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} antialiased`}>
         {children}
       </body>
     </html>
-  )
+  );
 }
